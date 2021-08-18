afe3006e556ed128d55f6ac2f0b22a56236158d7
cfranceschi-ledger <celia.franceschi-ext@ledger.fr>
2021-08-18 14:59:22 +0200

    first JS Implementation for active menu item during scroll. Needs work

---
+++ assets/js/scroll.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll("h2, h3");
+  const navLi = document.querySelectorAll(".sidebar-toc ul li");
+  window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLi.forEach((li) => {
+      li.classList.remove("active");
+      if (li.innerText.toUpperCase() == current.toUpperCase()) {
+        li.classList.add("active");
+      }
+    });
+  });
