a8c13a2e483c8361273edcdaae52adcbe9b674d5
cfranceschi-ledger <celia.franceschi-ext@ledger.fr>
2021-08-18 16:18:02 +0200

    wip scroll sidebar toc

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,11 +1,11 @@
 const sections = document.querySelectorAll("h2, h3");
-  const navLi = document.querySelectorAll(".sidebar-toc ul li");
+const navLi = document.querySelectorAll(".sidebar-toc ul li");
   window.addEventListener("scroll", () => {
     let current = "";
     sections.forEach((section) => {
       const sectionTop = section.offsetTop;
       const sectionHeight = section.clientHeight;
-      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      if (pageYOffset >= sectionTop - 200) {
         current = section.getAttribute("id");
       }
     });
