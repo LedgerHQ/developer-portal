d952e0ee2a7a78d92012dae2848a5c3abdfc30ad
cfranceschi-ledger <celia.franceschi-ext@ledger.fr>
2021-08-19 11:54:55 +0200

    javascript for scrolling table of content ok

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,10 +1,9 @@
-const sections = document.querySelectorAll("h2, h3");
+const sections = document.querySelectorAll("h2, h3, h4");
 const navLi = document.querySelectorAll(".sidebar-toc ul li");
   window.addEventListener("scroll", () => {
     let current = "";
     sections.forEach((section) => {
       const sectionTop = section.offsetTop;
-      const sectionHeight = section.clientHeight;
       if (pageYOffset >= sectionTop - 200) {
         current = section.getAttribute("id");
       }
@@ -12,7 +11,8 @@ const navLi = document.querySelectorAll(".sidebar-toc ul li");
 
     navLi.forEach((li) => {
       li.classList.remove("active");
-      if (li.innerText.toUpperCase() == current.toUpperCase()) {
+      formatedLi = li.innerText.toUpperCase().replaceAll(" ", "-").replaceAll(",", "").replaceAll("(", "").replaceAll(")", "");
+      if (formatedLi.split('\n')[0] == current.toUpperCase()) {
         li.classList.add("active");
       }
     });
