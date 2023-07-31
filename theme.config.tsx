d408df8545d8b078681e0fea0f1c74fb6bd3837d
cfranceschi-ledger <celia.franceschi@ledger.fr>
2023-07-31 10:23:52 +0200

    config

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,10 +9,29 @@ const config: DocsThemeConfig = {
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/LedgerHQ/developer-portal-v2',
   footer: {
     text: 'Nextra Docs Template',
   },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://developers.ledger.com/' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Developers'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Integrate your project with Ledger'}
+        />
+      </>
+    )
+  },
+  primaryHue: { dark: 120; light: 270 }
 }
 
 export default config
