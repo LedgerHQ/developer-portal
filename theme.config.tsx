cc68fb0794b12f41cf12959666a997e1efd66522
cfranceschi-ledger <celia.franceschi@ledger.fr>
2023-08-01 10:10:00 +0200

    changed structure

--- theme.config.tsx
+++ theme.config.tsx
@@ -16,6 +16,10 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Ledger Developer Portal',
   },
+  sidebar: {
+    autoCollapse: true,
+    defaultMenuCollapseLevel: 1
+  },
   head: () => {
     const { asPath, defaultLocale, locale } = useRouter()
     const { frontMatter } = useConfig()
@@ -38,9 +42,7 @@ const config: DocsThemeConfig = {
   logo: (
     <>
       <svg width="37" height="32" viewBox="0 0 37 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g clip-path="url(#clip0_1009_2494)">
         <path d="M0 22.9137V32H13.9173V29.9849H2.02778V22.9137H0ZM34.9722 22.9137V29.9849H23.0827V31.9995H37V22.9137H34.9722ZM13.9375 9.08631V22.9132H23.0827V21.0961H15.9653V9.08631H13.9375ZM0 0V9.08631H2.02778V2.01461H13.9173V0H0ZM23.0827 0V2.01461H34.9722V9.08631H37V0H23.0827Z" fill="currentColor"/>
-        </g>
       </svg>
       <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
         Developer Portal
