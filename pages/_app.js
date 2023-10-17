a26745be389429f600550bc9cd6108496e23f8f3
cfranceschi-ledger <celia.franceschi@ledger.fr>
2023-10-17 10:10:53 +0200

    added GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,14 @@
 import '../style/styles.css'
+import TagManager from 'react-gtm-module'
+
+
+const tagManagerArgs = {
+  gtmId: 'GTM-TRV7TV5M'
+}
+
+if (process.browser) {
+  TagManager.initialize(tagManagerArgs);
+}
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
