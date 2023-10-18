62c047bbfb0f19f37ae79ae9b3116db9bbb74034
Dorian De Simone <desimone.dorian@gmail.com>
2023-10-18 14:15:01 -0400

    numbers animation

--- components/Homepage/NumberItem.tsx
+++ components/Homepage/NumberItem.tsx
@@ -1,5 +1,10 @@
 import { FC, HTMLAttributes } from 'react'
 import { cn } from '../../lib/utils';
+import { useSpring, animated } from 'react-spring';
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 interface NumberItemProps extends HTMLAttributes<HTMLDivElement> {
   number: string;
@@ -7,9 +12,23 @@ interface NumberItemProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 export const NumberItem: FC<NumberItemProps> = ({number, text, className, ...props}) => {
+  const numberValue = Number(number.replace(/\D/g,''));
+  const nonStringValue = number.replace(/\d/g,'');
+
+  const spring = useSpring({ 
+    from: { val: 0 },
+    to: { val: numberValue },
+    delay: 200,
+    config: { mass:1, tension:20, friction: 10, duration: 3000 },
+  });
+
+
   return (
     <div className={cn("flex flex-col w-[350px] items-center gap-5 text-center", className)} {...props}>
-      <span className="font-mono-medium text-heading-5 lg:text-heading-3 text-purple">{number}</span>
+      <div className="font-mono-medium text-heading-5 lg:text-heading-3 text-purple">
+        <animated.span>{spring.val.to(val => (Math.floor(val)).toLocaleString("fr-FR"))}</animated.span>
+        {nonStringValue}
+      </div>
       <div className="text-p font-semibold">{text}</div>
     </div>
   )
