6a5641ca19936f3d79bacb8bcea778cd16d536be
fcipollone-ledger <74672573+fcipollone-ledger@users.noreply.github.com>
2023-10-19 14:34:17 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -12,4 +12,6 @@ export default withAuth(
   }
 )
 
+// 2023-10-19 - To access all protected pages locally, when the github app is not available, 
+// comment out the line below
 export const config = { matcher: ["/swap/:path*"] }
