0e9ec0473a8a14ec1cb4b3d564c625c963a794d6
lcastillo-ledger <78589616+lcastillo-ledger@users.noreply.github.com>
2023-10-19 14:07:00 +0200

    Add github authentication to the developer portal and protect exchange partners pages with it (#48)
    * Add authentication on Exchange documentation pages
    
    * Update pnpm lock file
    
    * Adding Paul Desreux to partners
    
    * Try to fix oauth errors

---
+++ middleware.js
@@ -0,0 +1,15 @@
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    console.log(req.nextauth.token)
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token?.role === "partner",
+    },
+  }
+)
+
+export const config = { matcher: ["/swap/:path*"] }
