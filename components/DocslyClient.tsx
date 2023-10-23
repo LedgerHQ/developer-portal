12ae827c2cd1284dcf5af638123407daecd5e810
cfranceschi-ledger <celia.franceschi@ledger.fr>
2023-10-23 16:12:09 +0200

    condition for docsly display

--- components/DocslyClient.tsx
+++ components/DocslyClient.tsx
@@ -5,5 +5,7 @@ import { usePathname } from "next/navigation";
  
 export default function DocslyClient() {
   const pathname = usePathname();
-  return <Docsly publicId="public_UDsp1PKk2nRwVMUdTRl1bWIk7mw26Div9V9IzTHx24wX5rP1r42UacubKadcffEc" pathname={pathname} />;
+  if (pathname !== '/' && pathname !== '/docs') {
+    return <Docsly publicId="public_UDsp1PKk2nRwVMUdTRl1bWIk7mw26Div9V9IzTHx24wX5rP1r42UacubKadcffEc" pathname={pathname} />;
+  }
 }
