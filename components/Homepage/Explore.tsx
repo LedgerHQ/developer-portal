7d3e40b8f030c6bbeedd6fadc6e90fbd3983cab6
cfranceschi-ledger <celia.franceschi@ledger.fr>
2023-10-26 14:43:52 +0200

    typo

--- components/Homepage/Explore.tsx
+++ components/Homepage/Explore.tsx
@@ -11,8 +11,7 @@ export const Explore = ({ journeyId }: ExploreProps) => {
         <div className="flex items-center justify-center px-4 py-20 md:px-28">
           <div className="flex flex-col items-center gap-16">
             <div className="text-heading-3 font-semibold tracking-tight-3 text-center hidden md:block">Explore the different developer journeys</div>
-            <div className="text-heading-3 font-semibold tracking-tight-2 text-center md:hidden">See how easy 
-            (and free) it is</div>
+            <div className="text-heading-3 font-semibold tracking-tight-2 text-center md:hidden">Explore the different developer journeys</div>
             <Button label="Get Started" type="primary" svgIcon="arrow-right" link={journeyId} />
           </div>
         </div>
