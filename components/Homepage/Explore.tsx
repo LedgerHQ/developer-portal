aa2ed37aa12dde8b5715a97f0a8046ccb7f03b93
Dorian De Simone <dorian.desimone-ext@ledger.fr>
2023-10-27 08:15:27 -0400

    title size updated (#59)

--- components/Homepage/Explore.tsx
+++ components/Homepage/Explore.tsx
@@ -10,8 +10,7 @@ export const Explore = ({ journeyId }: ExploreProps) => {
       <div className="relative max-w-[1440px] mx-auto bg-nano-background lg:bg-nano-background-lg xl:bg-nano-background-xl laptop:bg-nano-background-laptop bg-cover md:bg-contain bg-no-repeat bg-[40%] md:bg-right bg-black">
         <div className="flex items-center justify-center px-4 py-20 md:px-28">
           <div className="flex flex-col items-center gap-16">
-            <div className="text-heading-3 font-semibold tracking-tight-3 text-center hidden md:block">Explore the different developer journeys</div>
-            <div className="text-heading-3 font-semibold tracking-tight-2 text-center md:hidden">Explore the different developer journeys</div>
+            <div className="text-heading-5 md:text-heading-3 font-semibold tracking-tight-2 text-center">Explore the different developer journeys</div>
             <Button label="Get Started" type="primary" svgIcon="arrow-right" link={journeyId} />
           </div>
         </div>
