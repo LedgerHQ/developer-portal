103b1737acfadac5f55a615d4306c961efb8d336
cfranceschi-ledger <celia.franceschi@ledger.fr>
2023-11-06 17:25:03 +0100

    removed comment plugin from dev journey pages

--- components/DocslyClient.tsx
+++ components/DocslyClient.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
  
 export default function DocslyClient() {
   const pathname = usePathname();
-  if (pathname !== '/' && pathname !== '/docs') {
+  if (pathname !== '/' && !pathname.includes('dev-journey')) {
     return <Docsly publicId="public_UDsp1PKk2nRwVMUdTRl1bWIk7mw26Div9V9IzTHx24wX5rP1r42UacubKadcffEc" pathname={pathname} />;
   }
 }
