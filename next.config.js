af9cee7c3a4ceee78a2136d34d9c958d18d67396
cfranceschi-ledger <celia.franceschi@ledger.fr>
2023-11-07 14:28:46 +0100

    config start

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx'
-})
+});
 
-module.exports = withNextra()
+const redirects = {
+
+}
+
+module.exports = withNextra();
