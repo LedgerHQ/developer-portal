5accacf480c4efdde1a2246a1553608d4457c0b4
fcipollone-ledger <74672573+fcipollone-ledger@users.noreply.github.com>
2023-11-30 11:21:19 +0100

    update

--- components/Homepage/NumberItem.tsx
+++ components/Homepage/NumberItem.tsx
@@ -1,6 +1,6 @@
 import { FC, HTMLAttributes } from 'react'
 import { combination } from '../../lib/utils';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, animated, easings, } from 'react-spring';
 
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -11,17 +11,68 @@ interface NumberItemProps extends HTMLAttributes<HTMLDivElement> {
   text: string;
 }
 
-export const NumberItem: FC<NumberItemProps> = ({number, text, className, ...props}) => {
+export const NumberItem_1: FC<NumberItemProps> = ({number, text, className, ...props}) => {
   const numberValue = Number(number.replace(/\D/g,''));
   const nonStringValue = number.replace(/\d/g,'');
 
   const spring = useSpring({ 
-    from: { val: 0 },
-    to: { val: numberValue },
+    from: { val: numberValue*0 },
+    to: { val: numberValue*1 },
     delay: 200,
-    config: { mass:1, tension:20, friction: 10, duration: 3000 },
-  });
+    //config: { mass:4, tension:28, friction: 60, easing: easings.easeOutExpo },
 
+    config: { mass:3, tension:280, friction: 60, easing: easings.easeOutExpo },
+
+    //config: { mass:1, tension:280, friction: 70, easing: easings.easeOutExpo, duration: 3000},
+    //config: { mass:3, tension:280, friction: 70, easing: easings.easeOutCirc },
+  }
+  );
+
+  return (
+    <div className={combination("flex flex-col w-[320px] phone:w-[350px] items-center gap-5 text-center", className)} {...props}>
+      <div className="font-mono-medium text-heading-5 lg:text-heading-3 text-purple">
+        <animated.span>{spring.val.to(val => (Math.floor(val)).toLocaleString("fr-FR"))}</animated.span>
+        {nonStringValue}
+      </div>
+      <div className="text-p font-semibold">{text}</div>
+    </div>
+  )
+}
+
+export const NumberItem_2: FC<NumberItemProps> = ({number, text, className, ...props}) => {
+  const numberValue = Number(number.replace(/\D/g,''));
+  const nonStringValue = number.replace(/\d/g,'');
+
+  const spring = useSpring({ 
+    from: { val: numberValue*0 },
+    to: { val: numberValue*1 },
+    delay: 200,
+    config: { mass:3, tension:28, friction: 60, easing: easings.easeOutExpo },
+  }
+  );
+
+  return (
+    <div className={combination("flex flex-col w-[320px] phone:w-[350px] items-center gap-5 text-center", className)} {...props}>
+      <div className="font-mono-medium text-heading-5 lg:text-heading-3 text-purple">
+        <animated.span>{spring.val.to(val => (Math.floor(val)).toLocaleString("fr-FR"))}</animated.span>
+        {nonStringValue}
+      </div>
+      <div className="text-p font-semibold">{text}</div>
+    </div>
+  )
+}
+
+export const NumberItem_3: FC<NumberItemProps> = ({number, text, className, ...props}) => {
+  const numberValue = Number(number.replace(/\D/g,''));
+  const nonStringValue = number.replace(/\d/g,'');
+
+  const spring = useSpring({ 
+    from: { val: numberValue*0 },
+    to: { val: numberValue*1 },
+    delay: 200,
+    config: { mass:3, tension:280, friction: 87, easing: easings.easeOutExpo },
+  }
+  );
 
   return (
     <div className={combination("flex flex-col w-[320px] phone:w-[350px] items-center gap-5 text-center", className)} {...props}>
--- components/Homepage/Numbers.tsx
+++ components/Homepage/Numbers.tsx
@@ -1,4 +1,6 @@
-import { NumberItem } from "./NumberItem"
+import { NumberItem_1 } from "./NumberItem"
+import { NumberItem_2 } from "./NumberItem"
+import { NumberItem_3 } from "./NumberItem"
 
 const VerticalSeparator = () => (
   <div className="w-[1px] bg-grey-400 hidden md:block" />
@@ -13,13 +15,13 @@ export const Numbers = () => {
     <div className="relative bg-black">
       <div className="max-w-[1216px] mx-auto flex items-center justify-center px-4 py-16 md:px-28 md:py-20 bg-purple-radial-gradient-smaller-mobile md:bg-purple-radial-gradient-smaller">
         <div className="flex flex-col md:flex-row gap-6 md:gap-4 w-full items-center md:items-stretch place-content-between" id="overview">
-          <NumberItem number="140+" text="blockchain protocols secured" />
+          <NumberItem_1 number="170+" text="blockchain protocols secured" />
           <VerticalSeparator />
           <HorizontalSeparator />
-          <NumberItem number="200+" text="compatible DApps" />
+          <NumberItem_2 number="200+" text="compatible DApps" />
           <VerticalSeparator />
           <HorizontalSeparator />
-          <NumberItem number="6 000 000" text="devices sold worldwide" />
+          <NumberItem_3 number="6500000+" text="devices sold worldwide" />
         </div>
       </div>
     </div>
