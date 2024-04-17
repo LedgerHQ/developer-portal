93ff7f9eac85956f8fb40add0374cd99561f2500
cfranceschi-ledger <celia.franceschi@ledger.fr>
2024-04-17 09:31:00 +0200

    wording

--- theme.config.tsx
+++ theme.config.tsx
@@ -50,13 +50,13 @@ const config: DocsThemeConfig = {
  
     return (
       <>
-        {/* Google Tag Manager */}
+        {/* Google Tag Manager Header */}
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','${GTM_ID}');</script>
-        {/* End Google Tag Manager */}
+        {/* End Google Tag Manager Haeader */}
         <script type="text/javascript" src="https://cdn.cookielaw.org/consent/5ff3ecce-44bd-4ef3-b746-ec81546bf82a/OtAutoBlock.js" ></script>
           <script src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"  type="text/javascript" charSet="UTF-8" data-domain-script="5ff3ecce-44bd-4ef3-b746-ec81546bf82a" ></script>
           <script type="text/javascript">
