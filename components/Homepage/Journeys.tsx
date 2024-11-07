dad0d784bc124d470ea344ae8ee5683f92a8a008
cfranceschi-ledger <celia.franceschi@ledger.fr>
2024-11-07 15:49:34 +0100

    home

--- components/Homepage/Journeys.tsx
+++ components/Homepage/Journeys.tsx
@@ -8,24 +8,18 @@ export const Journeys = () => {
           <div className="text-heading-5 md:text-heading-3 tracking-tight-3 font-semibold" id="journeys">Documentation modules</div>
           <div className="flex flex-wrap gap-y-8 md:gap-x-[3%] mt-16">
 
-            <JourneyCard 
-              title="Clear Signing initiative"
-              description="Whitelist your smart contracts methods to enforce our new clear signing standard"
-              type="clear-signing"
-              className="w-full"
-            />
 
             <div className="text-heading-6 md:text-heading-5 tracking-tight-3 font-semibold w-full">Devices</div>
 
             <JourneyCard 
-              title="Develop an app for Ledger devices"
+              title="Device App Kit"
               description="Boilerplate apps and SDKs available in C and Rust to get your app in the Ledger Live 'My Ledger' Section"
               type="device"
               className="w-full md:w-[48.5%]"
             />
             <JourneyCard 
-              title="Get your app connected with Ledger devices"
-              description="Use the Device Management Kit to easily handle connection and communication between your app and Ledger devices" 
+              title="Device Management kit"
+              description="Easily handle connection and communication between your app and Ledger devices" 
               type="interaction"
               className="w-full md:w-[48.5%]"
             />
@@ -33,16 +27,25 @@ export const Journeys = () => {
             <div className="text-heading-6 md:text-heading-5 tracking-tight-3 font-semibold w-full">Ledger Live</div>
 
             <JourneyCard 
-              title="Add your Blockchain in Ledger Live"
+              title="Ledger Services Kit"
               description="Add the possibility to handle your Blockchain's accounts directly in Ledger Live" 
               type="blockchain"
               className="w-full md:w-[48.5%]"
             />
             <JourneyCard 
-              title="Get your app featured in the Discover section"
+              title="Ledger Services Kit"
               description="Extend the reach of your DApp by integrating it in Ledger Live" 
               type="live-app"
               className="w-full md:w-[48.5%]"
+            />
+
+            <div className="text-heading-6 md:text-heading-5 tracking-tight-3 font-semibold w-full">Clear signing</div>
+
+            <JourneyCard 
+              title="Clear Signing initiative"
+              description="Whitelist your smart contracts methods to enforce our new clear signing standard"
+              type="clear-signing"
+              className="w-full"
             />
 
           </div>
