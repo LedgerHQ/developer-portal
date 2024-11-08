6d058180aafbff6340fed5de145f2fdc23aaf5fe
Mathieu Bertin <mathieu.bertin@ledger.fr>
2024-11-08 15:59:51 +0100

    [DSDK-573] Add documentation for Device Management Kit

---
+++ pages/docs/device-interaction/integration/ldmk/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  docs: "Ledger Device Management Kits",
+  explanations: "Explanations",
+  beginners: "Beginner's guide",
+  integration_walkthroughs: "Integration Walkthrough",
+  migrations: "Migrations",
+  references: "References (TSDoc)",
+};
---
+++ pages/docs/device-interaction/integration/ldmk/explanations/_meta.js
@@ -0,0 +1,6 @@
+export default {
+  introduction: "Why the Device Management Kit?",
+  ledgerjs: "Differences with LedgerJS",
+  dmk: "Device Management Kit",
+  signers: "Signer kits",
+};
