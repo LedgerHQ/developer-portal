e5fd82ad2a1629dcb5180591e64350c88c0a7de8
cfranceschi-ledger <celia.franceschi@ledger.fr>
2025-04-15 10:27:47 +0200

    📝 (doc) [NO-ISSUE]: Update news

--- theme.config.tsx
+++ theme.config.tsx
@@ -12,7 +12,7 @@ const config: DocsThemeConfig = {
     key: "Developer Kits",
     content: (
       <a href="https://developers.ledger.com/docs/news" target="_blank">
-        ⚠️ Ledger is transitioning away from the Ledger Nano S. Read more →
+        📰 Ledger is transitioning away from the Ledger Nano S. Read more →
       </a>
     ),
   },
