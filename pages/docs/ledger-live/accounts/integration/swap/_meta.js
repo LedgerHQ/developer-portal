788aee167ed6a4f00ac842ef09606abb1296d8f7
Hedi EDELBLOUTE <hedi.edelbloute@gmail.com>
2025-05-09 17:28:25 +0200

    doc: update integration walkthrough

---
+++ pages/docs/ledger-live/accounts/integration/swap/_meta.js
@@ -0,0 +1,11 @@
+export default {
+  "before-you-start": "Before you start",
+  strategy: "Implementation strategy",
+  "sequence-diagram": "Sequence diagram",
+  "wallet-api": "1 - Wallet API",
+  "exchange-sdk": "2 - Exchange SDK",
+  "swap-live-app": "3 - Swap Live App",
+  "ledger-live": "4 - Ledger Live",
+  "test-plan": "Manual tests",
+  "qa-requirements": "Automated tests",
+};
