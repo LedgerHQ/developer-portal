2b994878ec8405ac2f42b946a0d65de1ac9b2a84
Francesco <74672573+fcipollone-ledger@users.noreply.github.com>
2025-05-22 17:25:58 +0200

    Doc/tuto clear signing 2 (#431)
    * Update pnpm-lock.yaml
    
    * doc/tuto-clear-signing-2
    
    This branch is based on the "doc/tuto-clear-signing" written by Paul Desreux

---
+++ pages/docs/clear-signing/integration/metadata-file/smart-contract/manual-creation/_meta.js
@@ -0,0 +1,9 @@
+export default {
+    "introduction": "Introduction",
+    "context": "1. Context",
+    "metadata": "2. Metadata",
+    "display": "3. Display",
+    "advanced": "4. Advanced",
+    "conclusion": "Conclusion",    
+    }
+    
